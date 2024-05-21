17b226fa9227991c40e69218fd391e38ecb3678d
Anya Aven <leafeon96@gmail.com>
2024-05-21 19:16:32 -0400

    Fixed bug in req.body

--- routes/orders.js
+++ routes/orders.js
@@ -3,6 +3,7 @@ import jsonschema from "jsonschema";
 import { BadRequestError } from "../expressError.js";
 import { shipViaShipIt } from "../shipItApi.js";
 import { getCost } from "../costs.js";
+import orderSchema from "../schemas/orderSchema.json" with {type: "json"};
 
 const router = new Router();
 
@@ -15,13 +16,22 @@ const router = new Router();
  */
 
 router.post("/:orderId/ship", async function (req, res) {
+  const result = jsonschema.validate(
+    req.body, orderSchema, { required: true });
+
+  console.log(result)
+  if (!result.valid) {
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
   const orderId = Number(req.params.orderId);
-  if (!req.body) throw new BadRequestError();
 
   const { productId, name, addr, zip } = req.body;
   const cost = getCost(productId);
   const trackingId = await shipViaShipIt(
     { orderId, productId, name, addr, zip });
+
   return res.json({ orderId, trackingId, cost });
 });
 
